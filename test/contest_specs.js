b0c5b8a4342f68280dbfa6570e30d6e28e813b62
ialcazar <israelalcazar@gmail.com>
2012-12-25 02:51:07 +0100

    First test fails

---
+++ test/contest_specs.js
@@ -0,0 +1,28 @@
+"use strict";
+
+
+var expect = require('expect.js'),
+    random = require('../lib/contest.js'),
+    sinon = require('sinon');
+
+
+describe( 'Contest' , function(){
+
+   beforeEach(function(){
+
+
+   });
+
+
+
+   it('Given an empty list Should return nobody', function(){
+        expect().fail();
+
+   });
+   xit('Given a list of particpants Should return a participant name', function(){
+
+        expect().fail();
+
+
+   });
+});
