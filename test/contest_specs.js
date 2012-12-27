916eb24d322965c180dc1970834b1d039afa03f5
Israel Alcazar Rodriguez <ialcazar@atsistemas.com>
2012-12-27 17:24:37 +0100

    Deleted some require modules

---
+++ test/contest_specs.js
@@ -0,0 +1,26 @@
+"use strict";
+
+
+var expect = require('expect.js');
+
+
+describe( 'Contest' , function(){
+
+   beforeEach(function(){
+
+
+   });
+
+
+
+   it('Given an empty list Should return nobody', function(){
+        expect().fail();
+
+   });
+   xit('Given a list of particpants Should return a participant name', function(){
+
+        expect().fail();
+
+
+   });
+});
